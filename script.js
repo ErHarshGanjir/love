9645af3bbf6b9214ff7a6b59343284ed797d7b74
harsh <204142915+ErHarshGanjir@users.noreply.github.com>
2026-02-03 15:19:20 +0530

    Update button behavior and message display

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
-const msg = document.getElementById("msg");
+const result = document.getElementById("result");
 
 noBtn.addEventListener("mouseover", () => {
-  const x = Math.random() * 300;
-  const y = Math.random() * 200;
+  const x = Math.random() * 200;
+  const y = Math.random() * 50;
 
   noBtn.style.left = x + "px";
   noBtn.style.top = y + "px";
 });
 
 yesBtn.addEventListener("click", () => {
-  msg.innerHTML = "I knew it! ❤️🥰";
+  result.innerHTML = "This was great 💖🥰";
 });
