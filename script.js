be727ce525c455a91e4582278c11e58181dc2177
harsh <204142915+ErHarshGanjir@users.noreply.github.com>
2026-02-03 15:44:23 +0530

    Rename closePopup to close and adjust event listener

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
 const popup = document.getElementById("popup");
-const closePopup = document.getElementById("closePopup");
+const close = document.getElementById("close");
 
 let scale = 1;
 
-// NO hover → move NO + grow YES
 noBtn.addEventListener("mouseenter", () => {
   const x = Math.random() * 80;
   const y = Math.random() * 20;
@@ -13,16 +12,14 @@ noBtn.addEventListener("mouseenter", () => {
   noBtn.style.left = x + "px";
   noBtn.style.top = y + "px";
 
-  scale += 0.2;
+  scale += 0.15;
   yesBtn.style.transform = `scale(${scale})`;
 });
 
-// YES click → popup
 yesBtn.addEventListener("click", () => {
   popup.style.display = "flex";
 });
 
-// Close popup
-closePopup.addEventListener("click", () => {
+close.addEventListener("click", () => {
   popup.style.display = "none";
 });
