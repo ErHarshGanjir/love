568ecdb5bd6869974f9450a076907f5a220b24e4
harsh <204142915+ErHarshGanjir@users.noreply.github.com>
2026-02-05 11:40:02 +0530

    Remove event listeners from noBtn and yesBtn

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-const noBtn = document.getElementById("noBtn");
-const yesBtn = document.getElementById("yesBtn");
-const result = document.getElementById("result");
 
-noBtn.addEventListener("mouseenter", () => {
-  const boxWidth = 80;
-  const boxHeight = 20;
-
-  const x = Math.random() * boxWidth;
-  const y = Math.random() * boxHeight;
-
-  noBtn.style.left = x + "px";
-  noBtn.style.top = y + "px";
-});
-
-yesBtn.addEventListener("click", () => {
-  result.textContent = "This was great 💖🥰";
-});
